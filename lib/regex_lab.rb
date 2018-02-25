ffee76a653a7a325244494321441a47779c4d5f5
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-02-25 18:44:43 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,9 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/^un.ing/)
+  if text.scan(/^un.ing/)
+    return true
+  end
 end
 
 def words_five_letters_long(text)
